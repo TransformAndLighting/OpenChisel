06fbf68592a1faf613ae3de8488f8c42e564922a
andrea <emb3d@transformandlighting.com>
2019-10-13 23:56:07 +0200

    ios comp

--- open_chisel/include/open_chisel/Chisel.h
+++ open_chisel/include/open_chisel/Chisel.h
@@ -45,12 +45,12 @@ namespace chisel
 
             void * operator new(size_t i)
             {
-                return _aligned_malloc(i, 64);
+                return std::aligned_malloc(i, 64); //_aligned_malloc(i, 64);
             }
 
             void operator delete(void * p)
             {
-                _aligned_free(p);
+				std::free(p);//_aligned_free(p);
             }
 
             Chisel();
